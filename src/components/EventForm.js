7db5f68b3c7da28747ee188ce9f739cdaeaaee94
devmullets <devmullets@gmail.com>
2020-06-06 21:53:23 -0400

    add AntD

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,11 +1,64 @@
 import React, { Component } from 'react';
+import { Button, Radio, Form, Input, Select, DatePicker, InputNumber, Switch, Checkbox } from 'antd';
+
+const layout = {
+    labelCol: {
+      span: 8,
+    },
+    wrapperCol: {
+      span: 16,
+    },
+  };
+  const tailLayout = {
+    wrapperCol: {
+      offset: 8,
+      span: 16,
+    },
+  };
 
 class EventForm extends Component {
     render() {
         return (
-            <>
-                
-            </>
+            <Form
+                {...layout}
+                name="basic"
+            >
+            <Form.Item
+                label="Username"
+                name="username"
+                rules={[
+                {
+                    required: true,
+                    message: 'Please input your username!',
+                },
+                ]}
+            >
+                <Input />
+            </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
         );
     }
 }
---
+++ src/containers/LandingPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import EventForm from '../components/EventForm';
+
+class LandingPage extends Component {
+    render() {
+        return (
+            <EventForm />
+        );
+    }
+}
+
+export default LandingPage;
